fcaa156920b3323442899dabdc86642b8efb90f0
StudentAD <79465687+StudentAD@users.noreply.github.com>
2023-11-29 15:42:53 +0100

    Add files

--- PadelPro/app/src/main/java/edu/ap/padelpro/MainActivity.kt
+++ PadelPro/app/src/main/java/edu/ap/padelpro/MainActivity.kt
@@ -6,6 +6,7 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -13,24 +14,43 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import edu.ap.padelpro.databinding.ActivityMainBinding
+import androidx.fragment.app.FragmentManager
 import edu.ap.padelpro.ui.theme.PadelProTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
 
-    private lateinit var button: Button
+    private lateinit var binding: ActivityMainBinding
+
+    //private lateinit var button: Button
     private lateinit var auth: FirebaseAuth
-    private lateinit var textView: TextView
+    //private lateinit var textView: TextView
     private lateinit var user: FirebaseUser
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.play -> replaceFragment(Play())
+                R.id.profile -> replaceFragment(Profile())
+                R.id.settings -> replaceFragment(Settings())
+
+                else -> {
+
+                }
+            }
+            true
+        }
 
         auth = FirebaseAuth.getInstance()
-        button = findViewById(R.id.logout)
-        textView = findViewById(R.id.user_details)
+        //button = findViewById(R.id.logout)
+        //textView = findViewById(R.id.user_details)
         user = auth.currentUser!!
         if (user == null) {
             val intent = Intent(applicationContext, Login::class.java)
@@ -38,15 +58,22 @@ class MainActivity : ComponentActivity() {
             finish()
         }
         else {
-            textView.setText(user.email)
+            //textView.setText(user.email)
         }
 
-        button.setOnClickListener {
+        /*button.setOnClickListener {
             FirebaseAuth.getInstance().signOut()
             val intent = Intent(applicationContext, Login::class.java)
             startActivity(intent)
             finish()
-        }
+        }*/
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
     }
 }
 
