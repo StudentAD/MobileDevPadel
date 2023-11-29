3d41ca61527b569b1ac608430717c3b859a00338
Alan <s124568@ap.be>
2023-11-29 21:53:53 +0100

    Edit profile display data

--- PadelPro/app/src/main/java/edu/ap/padelpro/Profile.kt
+++ PadelPro/app/src/main/java/edu/ap/padelpro/Profile.kt
@@ -10,12 +10,15 @@ import android.widget.Button
 import android.widget.TextView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.FirebaseFirestore
 
 class Profile : Fragment() {
 
     private lateinit var buttonLogout: Button
-    private lateinit var textViewUserDetails: TextView
+    private lateinit var textViewUserName: TextView
+    private lateinit var textViewCity: TextView
     private lateinit var auth: FirebaseAuth
+    private lateinit var db: FirebaseFirestore
     private var currentUser: FirebaseUser? = null
 
     override fun onCreateView(
@@ -26,15 +29,40 @@ class Profile : Fragment() {
         val view = inflater.inflate(R.layout.fragment_profile, container, false)
 
         auth = FirebaseAuth.getInstance()
+        db = FirebaseFirestore.getInstance()
         buttonLogout = view.findViewById(R.id.logout_button)
-        textViewUserDetails = view.findViewById(R.id.user_details_text)
+        textViewUserName = view.findViewById(R.id.user_name_text)
+        textViewCity = view.findViewById(R.id.city_text)
 
         // Retrieve the current user from MainActivity (assuming it's already authenticated)
         currentUser = (requireActivity() as MainActivity).getCurrentUser()
 
         // Display user details if the currentUser is not null
         currentUser?.let { user ->
-            textViewUserDetails.text = "User Email: ${user.email}"
+            val userID = user.uid
+            val userRef = db.collection("users").document(userID)
+
+            userRef.get()
+                .addOnSuccessListener { documentSnapshot ->
+                    if (documentSnapshot.exists()) {
+                        val userData = documentSnapshot.data
+                        val firstName = userData?.get("firstName") as? String ?: "No first name"
+                        val lastName = userData?.get("lastName") as? String ?: "No last name"
+                        val city = userData?.get("city") as? String ?: "No city"
+
+                        val userName = "$firstName $lastName"
+                        textViewUserName.text = userName
+                        textViewCity.text = "City: $city"
+                    } else {
+                        textViewUserName.text = "User details not available"
+                        textViewCity.text = "City details not available"
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    // Handle failures
+                    textViewUserName.text = "Failed to retrieve user details"
+                    textViewCity.text = "Failed to retrieve city details"
+                }
         }
 
         buttonLogout.setOnClickListener {
